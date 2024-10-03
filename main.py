c417cd5f383bbde7c12de49c8691805ce32df428
smetana mikulas <smetana.mikulas>
2024-10-03 10:28:34 +0200

    try except

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def obsah():
-    a = int(input("Zadej velikost strany a v cm: "))
-    b = int(input("Zadej velikost strany b v cm: "))
-    if(a>0 and b>0):
-        obsah = a*b
-        print(f"Obsah obdélníku o straně a {a}cm a b {b} je {obsah}.")
-    else:
-        print("Špatné hodnoty, zkuste to znovu.")
-        obsah()
+    while True:
+        try:    
+            a = int(input("Zadej velikost strany a v cm: "))
+            b = int(input("Zadej velikost strany b v cm: "))
+            if(a>0 and b>0):
+                obsah = a*b
+                print(f"Obsah obdélníku o straně {a}cm a {b}cm je {obsah}.")
+            else:
+                print("Špatné hodnoty, zkuste to znovu.")
+        except:
+            print("Špatné hodnoty, zkuste to znovu.")
+        else:
+            break
 
 obsah()
