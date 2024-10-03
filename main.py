35afc960d3d6d950a0837f598dcda33ed2d07694
smetana mikulas <smetana.mikulas>
2024-10-03 10:20:54 +0200

    rekurze

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def obsah():
     a = int(input("Zadej velikost strany a v cm: "))
     b = int(input("Zadej velikost strany b v cm: "))
-    obsah = a*b
-    print(f"Obsah obdélníku o straně a {a}cm a b {b} je {obsah}.")
+    if(a>0 and b>0):
+        obsah = a*b
+        print(f"Obsah obdélníku o straně a {a}cm a b {b} je {obsah}.")
+    else:
+        print("Špatné hodnoty, zkuste to znovu.")
+        obsah()
 
-while True:
-    obsah()
+obsah()
